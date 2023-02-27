05f9cc730e39bba2bd51f69f0ec5695a1bef0bc2
IsaacKetto <gylleneisaac@gmail.com>
2023-02-27 10:05:58 +0100

    Almost fixed simple routes

--- lib/response.rb
+++ lib/response.rb
@@ -1,39 +1,55 @@
+require 'mime/types'
+
 class Response
-    def initialize(session, data)
+    def initialize(session, request)
         @session = session
-        @data = data
-        run
+        @request = request 
+        @file_type, @file_route, @resource_type = resource(@request)
+        @resource_file, @status, @file_size = file_check(@request, @file_route, @file_type, @resource_type)
+        session_print(@session)
     end
 
+    #fixa så att file_route hittar filerna
     private
-    def run
+    def resource(request)
+        file_type = (request.resource).split('.')[1]
+        if file_type == nil
+            file_type = 'html'
+            file_route = request.resource + '.' + file_type
+            resource_type = MIME::Types.type_for(file_route).first
+        else
+            file_route = file_type + request.resource
+            resource_type = MIME::Types.type_for(file_route).first
+        end
+        p file_type
+        p resource_type
+        return file_type, file_route, resource_type
+    end
 
-        request = Request.new(@data) 
-
-        resource_type = MIME::Types.type_for('html' + request.resource).first
-        resource_media_type = resource_type.media_type
-        resource_sub_type = resource_type.sub_type
-
-        if File.exist?('html' + request.resource)
+    def file_check(request, file_route, file_type, resource_type)
+        if File.exist?(file_route)
             status = 200
-            case resource_media_type
+            case resource_type.media_type
             when "text", "application"
-                resource_file = File.read('html' + request.resource)
+                resource_file = File.read(file_route)
                 file_size = resource_file.size
             when "image"
-                resource_file = File.binread('html' + request.resource)
+                resource_file = File.binread(file_route)
                 file_size = resource_file.bytesize
             end
         else
             status = 404
             resource_file = "ERROR 404 NOT FOUND"
         end
+        return resource_file, status, file_size
+    end
 
-        @session.print "HTTP/1.1 #{status}\r\n"
-        @session.print "Content-Type: #{resource_media_type}/#{resource_sub_type}\r\n"
-        @session.print "Content-length: #{file_size}\r\n"
-        @session.print "\r\n"
-        @session.print resource_file
-        @session.close
+    def session_print(session)
+        session.print "HTTP/1.1 #{@status}\r\n"
+        session.print "Content-Type: #{@resource_type.media_type}/#{@resource_type.sub_type}\r\n"
+        session.print "Content-length: #{@file_size}\r\n"
+        session.print "\r\n"
+        session.print @resource_file
+        session.close
     end
 end
